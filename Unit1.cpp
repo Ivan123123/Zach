63c284288da1d7a5588585de2a469a36933e1452
Ivan123123 <ivanivanbo@mail.ru>
2016-12-05 16:46:18 +0300

    v3

--- Unit1.cpp
+++ Unit1.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main()
 {
 
+        int gl=0, sg=0, i, j;
+        char gls[]="qwrtpsdfghjklzxcvbnm";
+        char str[100];
+        cout<<"Enter string: ";
+        cin>>str;
+        for(i=0; i<(strlen(str)); i++)
+                for(j=0; j<strlen(gls); j++)
+                        if(str[i]==gls[j])
+                                gl++;
+        cout<<endl<<"Soglasnyh bukv - "<<gl<<endl;
         system("pause");
         return 0;
 }
